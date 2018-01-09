931d0275b83a7b6f46bb0fbf3a0f444a57d28db5
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2018-01-09 17:41:08 +0300

    Fix possible NPE in IdentityResolver when some value in chain are null

--- src/main/java/com/intellij/debugger/streams/resolve/IdentityResolver.java
+++ src/main/java/com/intellij/debugger/streams/resolve/IdentityResolver.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.debugger.streams.resolve;
 
 import com.intellij.debugger.streams.trace.TraceElement;
@@ -31,6 +17,8 @@ import java.util.Map;
  * @author Vitaliy.Bibaev
  */
 public class IdentityResolver implements ValuesOrderResolver {
+  private static final Object NULL_MARKER = new Object();
+
   @NotNull
   @Override
   public Result resolve(@NotNull TraceInfo info) {
@@ -44,10 +32,10 @@ public class IdentityResolver implements ValuesOrderResolver {
       .of(after.keySet())
       .sorted()
       .map(after::get)
-      .groupingBy(TraceUtil::extractKey);
+      .groupingBy(IdentityResolver::extractKey);
 
     for (final TraceElement element : before.values()) {
-      final Object value = TraceUtil.extractKey(element);
+      final Object value = extractKey(element);
 
       final List<TraceElement> elements = grouped.get(value);
       if (elements == null || elements.isEmpty()) {
@@ -64,5 +52,11 @@ public class IdentityResolver implements ValuesOrderResolver {
     }
 
     return Result.of(direct, reverse);
+  }
+
+  @NotNull
+  private static Object extractKey(@NotNull TraceElement element) {
+    final Object key = TraceUtil.extractKey(element);
+    return key == null ? NULL_MARKER : key;
   }
 }
